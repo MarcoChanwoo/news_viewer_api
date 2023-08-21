793d58934c997e8dcaefc065b6c64ec6274a660c
MarcoChanwoo <cwhong8087@gmail.com>
2023-08-21 22:33:41 +0900

    1st update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [data, setData] = useState(null);
+    const onClick = async () => {
+        try {
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/todos/1',
+            );
+            setData(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    return (
+        <div>
+            <div>
+                <button onClick={onClick}>불러오기</button>
+            </div>
+            {data && (
+                <textarea
+                    rows={7}
+                    value={JSON.stringify(data, null, 2)}
+                    readOnly={true}
+                />
+            )}
+        </div>
+    );
+};
 
 export default App;
