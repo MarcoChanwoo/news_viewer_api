3c21b2b9d01dc15f2b7f88d638541fa581cd5a9b
MarcoChanwoo <cwhong8087@gmail.com>
2023-08-22 15:26:23 +0900

    3rd update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
-import { useCallback, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-    const [category, setCategory] = useState('all');
-    const onSelect = useCallback((category) => setCategory(category), []);
-
     return (
-        <>
-            <Categories category={category} onSelect={onSelect} />
-            <NewsList category={category} />;
-        </>
+        <Routes>
+            <Route path="/" element={<NewsPage />} />
+            <Route path="/:category" element={<NewsPage />} />
+        </Routes>
     );
 };
 
